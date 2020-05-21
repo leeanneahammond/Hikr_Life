ad7b16503d99239cd4296c1dff4002d638f887a3
Leeanne Hammond <leeannehammond@yahoo.com>
2020-05-21 18:33:24 -0400

    backend almost done

--- server.js
+++ server.js
@@ -3,16 +3,21 @@ require("dotenv").config(); // for loading environment variables
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
+const passport = require("passport");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const MONGO_URI = process.env.MONGO_URI;
-
 mongoose
 .connect(MONGO_URI, { useNewUrlParser: true })
 .then(() => console.log("Mongo Connection successful"))
 .catch(err => console.log("err"));
+
+app.use(passport.initialize());
+require("./middleware/passport")(passport);
+app.use("/api/users/", require("./routes/api/users"));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
