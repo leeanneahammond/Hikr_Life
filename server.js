9983ab3c2f4a986e5be89a8073800396d72a59d3
Leeanne Hammond <leeannehammond@yahoo.com>
2020-05-22 16:20:08 -0400

    widget

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-require("dotenv").config(); // for loading environment variables
+require("dotenv").config(); 
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
