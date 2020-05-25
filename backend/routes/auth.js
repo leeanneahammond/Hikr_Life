ae8bc887a6cf24a42e754a1e95ff8fc6ecf96229
leeanneahammond <56902098+leeanneahammond@users.noreply.github.com>
2020-05-25 09:30:45 -0400

    delete

--- backend/routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { time } = require('../controllers/auth');
-
-router.post('/signup', signup);
-
-module.exports = router;
