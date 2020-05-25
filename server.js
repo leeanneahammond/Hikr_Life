6a637da3e433ad9dc6305924304fc93fdc8f0ef1
Leeanne Hammond <leeannehammond@yahoo.com>
2020-05-24 21:23:34 -0400

    update routes

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
+// bring routes
+const blogRoutes = require('./routes/blog');
 
 // app
 const app = express();
 
 // DB
-mongoose.connect(process.env.DATABASE, {useNewUrlParser: true, useCreateIndex: true, 
+mongoose.connect(process.env.DATABASE_LOCAL, {useNewUrlParser: true, useCreateIndex: true, 
     useFindAndModify: false, useUnifiedTopology: true})
     .then(() => console.log('DB connected'))
     .catch(err => {
@@ -26,10 +28,9 @@ app.use(cookieParser());
 if(process.env.NODE_ENV == 'development') {
     app.use(cors({origin: `${process.env.CLIENT_URL}`}));
 }
-// routes
-app.get('/api', (req, res) => {
-    res.json({time: Date().toString()})
-})
+
+// route middleware
+app.use('/api', blogRoutes);
 
 // port
 const port = process.env.PORT || 3000;
